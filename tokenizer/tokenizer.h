f67010a2748145c4e0370cf84894e8de5803b983
marhiru <marcosgamern@gmail.com>
2026-01-18 23:59:47 -0300

    feat: create a pattern to `tokenizer.h`

--- tokenizer/tokenizer.h
+++ tokenizer/tokenizer.h
@@ -4,63 +4,60 @@
 #include <stddef.h>
 #define MODAL_VERSION "0.0.1"
 
+// Token kinds - what the token represents
 typedef enum {
   TOK_EOF,
-  LPAREN,
-  RPAREN,
-  LBRACE,
-  RBRACE,
-  OPERATOR,
-  IDENTIFIER,
-  UNKNOWN,
-  NUMBER,
-  TEST,
-  ASSERT,
-  SIZEOF,
-  DEFER,
-  AUTOFREE,
-  ALIAS,
-  USE,
-  COMPTIME,
-  UNION,
-  ASM,
-  VOLATILE,
-  ASYNC,
-  AWAIT,
-  AND,
-  OR,
-  Q_DOT,
-  QQ_EQ,
-  QQ,
-  QUESTION,
-  PIPE,
-  DCOLON,
-  ELLIPSIS,
-  DOTDOT,
-  ARROW,
-  STRING,
-} Kind;
+  TOK_LPAREN,
+  TOK_RPAREN,
+  TOK_LBRACE,
+  TOK_RBRACE,
+  TOK_OPERATOR,
+  TOK_IDENTIFIER,
+  TOK_UNKNOWN,
+  TOK_NUMBER,
+  TOK_STRING,
+  TOK_PREPROC,
+  // Keywords
+  TOK_TEST,
+  TOK_ASSERT,
+  TOK_SIZEOF,
+  TOK_DEFER,
+  TOK_AUTOFREE,
+  TOK_ALIAS,
+  TOK_USE,
+  TOK_COMPTIME,
+  TOK_UNION,
+  TOK_ASM,
+  TOK_VOLATILE,
+  TOK_ASYNC,
+  TOK_AWAIT,
+  TOK_AND,
+  TOK_OR,
+  // Multi-char operators
+  TOK_Q_DOT,
+  TOK_QQ_EQ,
+  TOK_QQ,
+  TOK_QUESTION,
+  TOK_PIPE,
+  TOK_DCOLON,
+  TOK_ELLIPSIS,
+  TOK_DOTDOT,
+  TOK_ARROW,
+} TokenKind;
 
+// Lexer states - internal scanning state machine
 typedef enum {
-  START,
-  EXPECT_NEWLINE,
-  INVALID,
-  CHAR,
-  INT,
-  FLOAT,
-  FSTRING,
-  STRING_LIT,
-  STATE_IDENTIFIER,
-  BLOCK_COMMENT,
-  LINE_COMMENT,
-  PREPROC,
-  IF,
-  WHILE,
-  FOR,
-} State;
+  LEX_STATE_START,
+  LEX_STATE_IDENTIFIER,
+  LEX_STATE_NUMBER_INT,
+  LEX_STATE_NUMBER_FLOAT,
+  LEX_STATE_STRING_LIT,
+  LEX_STATE_LINE_COMMENT,
+  LEX_STATE_BLOCK_COMMENT,
+} LexerState;
 
 typedef struct {
-  Kind kind;
+  TokenKind kind;
   const char *start;
   int len;
   int line;
@@ -72,17 +69,54 @@ typedef struct {
   int pos;
   int line;
   int col;
-  State state;
+  LexerState state;
 } Tokenizer;
 
 typedef struct {
   const char *kw;
   size_t len;
-  Kind kind;
+  TokenKind kind;
 } Keyword;
 
-Token token_make(Kind kind, const char *start, int len, int line, int col);
+Token token_make(TokenKind kind, const char *start, int len, int line, int col);
 void init(Tokenizer *t, const char *buffer);
 Token next(Tokenizer *t);
+
+// Legacy aliases for compatibility - can be removed once parser is updated
+#define Kind TokenKind
+#define LPAREN TOK_LPAREN
+#define RPAREN TOK_RPAREN
+#define LBRACE TOK_LBRACE
+#define RBRACE TOK_RBRACE
+#define OPERATOR TOK_OPERATOR
+#define IDENTIFIER TOK_IDENTIFIER
+#define UNKNOWN TOK_UNKNOWN
+#define NUMBER TOK_NUMBER
+#define STRING TOK_STRING
+#define TEST TOK_TEST
+#define ASSERT TOK_ASSERT
+#define SIZEOF TOK_SIZEOF
+#define DEFER TOK_DEFER
+#define AUTOFREE TOK_AUTOFREE
+#define ALIAS TOK_ALIAS
+#define USE TOK_USE
+#define COMPTIME TOK_COMPTIME
+#define UNION TOK_UNION
+#define ASM TOK_ASM
+#define VOLATILE TOK_VOLATILE
+#define ASYNC TOK_ASYNC
+#define AWAIT TOK_AWAIT
+#define AND TOK_AND
+#define OR TOK_OR
+#define Q_DOT TOK_Q_DOT
+#define QQ_EQ TOK_QQ_EQ
+#define QQ TOK_QQ
+#define QUESTION TOK_QUESTION
+#define PIPE TOK_PIPE
+#define DCOLON TOK_DCOLON
+#define ELLIPSIS TOK_ELLIPSIS
+#define DOTDOT TOK_DOTDOT
+#define ARROW TOK_ARROW
+#define PREPROC TOK_PREPROC
 
 #endif
