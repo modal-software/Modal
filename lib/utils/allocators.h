3de0cd5fbb95bdb2aab0c74662e8becdb5ee1671
marhiru <marcosgamern@gmail.com>
2026-01-23 22:29:01 -0300

    feat: adding allocators utils header

--- lib/utils/allocators.h
+++ lib/utils/allocators.h
@@ -1,9 +1,17 @@
 #ifndef ALLOCATORS_H
 #define ALLOCATORS_H
+#include <stddef.h>
 
-#define free(ptr) ((void)0)
-#define malloc(sz) xmalloc(sz)
-#define realloc(p, s) xrealloc(p, s)
-#define calloc(n, s) xcalloc(n, s)
+#define BLOCK_SIZE (1024 * 1024)
+
+typedef struct ArenaBlock
+{
+    struct ArenaBlock *next;
+    size_t used;
+    size_t cap;
+    char data[];
+} ArenaBlock;
+
+void *arena_alloc_raw(size_t size);
 
 #endif
