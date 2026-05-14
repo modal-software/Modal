a88408053a1a63bf495861799653f683c3d1bb13
marhiru <marcosgamern@gmail.com>
2026-05-14 03:03:50 -0300

    fix: formatted file

--- lib/compiler/ast/ast.c
+++ lib/compiler/ast/ast.c
@@ -4,98 +4,87 @@
 #include <stdlib.h>
 #include <string.h>
 
-AstNode *ast_new_number(Token tok, long long val)
-{
+AstNode *ast_new_number(Token tok, long long val) {
     AstNode *node = malloc(sizeof(AstNode)); // aloca nó base
-    if (!node)
-    {
-        return NULL;
-    }
-    *node = (AstNode){.kind = AST_NUMBER_LIT, .token = tok, .data = {.number = {val}}};
-    return node;
-}
-
-AstNode *ast_new_ident(Token tok)
-{
-    AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
     *node = (AstNode){
-        .kind = AST_IDENT, .token = tok, .data = {.ident = {.name = tok.start, .len = tok.len}}};
+        .kind = AST_NUMBER_LIT, .token = tok, .data = {.number = {val}}};
     return node;
 }
 
-AstNode *ast_new_binop(Token op_tok, AstNode *left, AstNode *right)
-{
+AstNode *ast_new_ident(Token tok) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
-    *node = (AstNode){
-        .kind = AST_BIN_OP, .token = op_tok, .data = {.binop = {left, right, op_tok.kind}}};
+    *node = (AstNode){.kind = AST_IDENT,
+                      .token = tok,
+                      .data = {.ident = {.name = tok.start, .len = tok.len}}};
     return node;
 }
 
-AstNode *ast_new_group(Token open_tok, AstNode **stmts, size_t count)
-{
+AstNode *ast_new_binop(Token op_tok, AstNode *left, AstNode *right) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
+        return NULL;
+    }
+    *node = (AstNode){.kind = AST_BIN_OP,
+                      .token = op_tok,
+                      .data = {.binop = {left, right, op_tok.kind}}};
+    return node;
+}
+
+AstNode *ast_new_group(Token open_tok, AstNode **stmts, size_t count) {
+    AstNode *node = malloc(sizeof(AstNode));
+    if (!node) {
         return NULL;
     }
 
     AstNode **children = malloc(count * sizeof(AstNode *));
-    if (!children)
-    {
+    if (!children) {
         free(node);
         return NULL;
     }
     memcpy(children, stmts, count * sizeof(AstNode *));
 
-    *node = (AstNode){
-        .kind = AST_PAREN_GROUP, .token = open_tok, .data = {.block_or_group = {children, count}}};
+    *node = (AstNode){.kind = AST_PAREN_GROUP,
+                      .token = open_tok,
+                      .data = {.block_or_group = {children, count}}};
     return node;
 }
 
-AstNode *ast_new_block(Token open_tok, AstNode **stmts, size_t count)
-{
+AstNode *ast_new_block(Token open_tok, AstNode **stmts, size_t count) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
 
     AstNode **children = malloc(count * sizeof(AstNode *));
-    if (!children)
-    {
+    if (!children) {
         free(node);
         return NULL;
     }
     memcpy(children, stmts, count * sizeof(AstNode *));
 
-    *node = (AstNode){
-        .kind = AST_BLOCK, .token = open_tok, .data = {.block_or_group = {children, count}}};
+    *node = (AstNode){.kind = AST_BLOCK,
+                      .token = open_tok,
+                      .data = {.block_or_group = {children, count}}};
     return node;
 }
 
-AstNode *ast_new_break_label(Token token)
-{
-    if (token.kind == TOK_COLON)
-    {
+AstNode *ast_new_break_label(Token token) {
+    if (token.kind == TOK_COLON) {
         return NULL;
     }
 
     return NULL;
 }
 
-AstNode *ast_new_assert(AstNode *expr)
-{
+AstNode *ast_new_assert(AstNode *expr) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
     *node = (AstNode){.kind = AST_ASSERT_STMT,
@@ -104,27 +93,24 @@ AstNode *ast_new_assert(AstNode *expr)
     return node;
 }
 
-AstNode *ast_new_string(Token tok)
-{
+AstNode *ast_new_string(Token tok) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
     const char *string_value = tok.start + 1;
     size_t string_len = tok.len - 2;
 
-    *node = (AstNode){.kind = AST_STRING_LIT,
-                      .token = tok,
-                      .data = {.string = {.value = string_value, .len = string_len}}};
+    *node = (AstNode){
+        .kind = AST_STRING_LIT,
+        .token = tok,
+        .data = {.string = {.value = string_value, .len = string_len}}};
     return node;
 }
 
-AstNode *ast_new_test(Token token, AstNode *block)
-{
+AstNode *ast_new_test(Token token, AstNode *block) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
 
@@ -142,30 +128,26 @@ AstNode *ast_new_test(Token token, AstNode *block)
     return node;
 }
 
-AstNode *ast_new_write(AstNode *group)
-{
+AstNode *ast_new_write(AstNode *group) {
     AstNode *node = malloc(sizeof(AstNode));
-    if (!node)
-    {
+    if (!node) {
         return NULL;
     }
 
-    *node = (AstNode){.kind = AST_WRITE_STMT,
-                      .token = group->token,
-                      .data = {.write = {.group = group, .len = group->token.len}}};
+    *node =
+        (AstNode){.kind = AST_WRITE_STMT,
+                  .token = group->token,
+                  .data = {.write = {.group = group, .len = group->token.len}}};
 
     return node;
 }
 
-void ast_free(AstNode *node)
-{
-    if (!node)
-    {
+void ast_free(AstNode *node) {
+    if (!node) {
         return;
     }
 
-    switch (node->kind)
-    {
+    switch (node->kind) {
     case AST_BIN_OP:
         ast_free(node->data.binop.left);
         ast_free(node->data.binop.right);
@@ -175,8 +157,7 @@ void ast_free(AstNode *node)
         break;
     case AST_BLOCK:
     case AST_PAREN_GROUP:
-        for (size_t i = 0; i < node->data.block_or_group.count; i++)
-        {
+        for (size_t i = 0; i < node->data.block_or_group.count; i++) {
             ast_free(node->data.block_or_group.stmts[i]);
         }
         free(node->data.block_or_group.stmts);
